82f9898d33f99c909d27b1869d4be11f635ec2c6
Nicolas Brunie <nicolas.brunie@sifive.com>
2021-05-13 13:38:50 -0700

    critical-path: prototyping latency evaluator

--- lib/Dialect/FIRRTL/Transforms/CriticalPathAnalysis.cpp
+++ lib/Dialect/FIRRTL/Transforms/CriticalPathAnalysis.cpp
@@ -34,7 +34,8 @@ namespace {
 class CriticalPathAnalysisPass : public CriticalPathAnalysisBase<CriticalPathAnalysisPass> {
   void runOnOperation() override;
 
-  llvm::DenseMap<Operation*, double> opLatency;
+  // llvm::DenseMap<Operation, double> opsLatency;
+  llvm::DenseMap<Value, double> valuesLatency;
 
   /// Whether any checks have failed.
   bool anyFailed;
@@ -46,7 +47,7 @@ void CriticalPathAnalysisPass::runOnOperation() {
   anyFailed = false;
 
   LLVM_DEBUG(llvm::dbgs()
-             << "executing CriticalPathAnalysisPass.\n");
+             << "executing CriticalPathAnalysisPass on " << module.getName().str() << ".\n");
 
   // Check the port types. Unfortunately we don't have an exact location of the
   // port name, so we just point at the module with the port name mentioned in
@@ -55,55 +56,114 @@ void CriticalPathAnalysisPass::runOnOperation() {
     LLVM_DEBUG(llvm::dbgs()
                << "Port " << port.name << " of type " << port.type << "\n");
   }
-  return;
 
   std::list<Operation*> worklist;
   double maxOpLatency = 0.0;
   Operation* criticalPathEnd = nullptr;
 
+  struct ExprInitialLatencyEvaluator : public FIRRTLVisitor<ExprInitialLatencyEvaluator, double> {
+    using FIRRTLVisitor<ExprInitialLatencyEvaluator, double>::visitExpr;
+
+    double visitInvalidExpr(Operation *op) { return -1.0; }
+    double visitUnhandledExpr(Operation *op) { return -1.0; }
+    // constant operation have zero-latency
+    double visitExpr(ConstantOp op) {return 0.0; }
+  };
+
+  auto initialEvaluator = ExprInitialLatencyEvaluator();
+
+
   // Check the results of each operation.
   module->walk([&](Operation *op) {
-    opLatency[op] = -1.0;
-    worklist.push_back(op);
+    LLVM_DEBUG(llvm::dbgs()
+               << "Found op  " << op->getName().getStringRef().str() << "\n");
+    double latency = initialEvaluator.dispatchExprVisitor(op);
+    LLVM_DEBUG(llvm::dbgs()
+               << "latency for op  " << op->getName().getStringRef().str() << " has been evaluated to " << latency << "\n");
+    // opsLatency[op] = latency;
+    valuesLatency[op->getOpResult(0)] = latency;
+    // enqueue operations for which latency was not determined
+    if (latency < 0)
+      worklist.push_back(op);
   });
+  struct ExprLatencyEvaluator : public FIRRTLVisitor<ExprLatencyEvaluator, bool, llvm::DenseMap<Value, double>> {
+    using FIRRTLVisitor<ExprLatencyEvaluator, bool, llvm::DenseMap<Value, double>>::visitExpr;
+    using FIRRTLVisitor<ExprLatencyEvaluator, bool, llvm::DenseMap<Value, double>>::visitStmt;
+
+    bool visitInvalidExpr(Operation *op, llvm::DenseMap<Value, double> latencyMap) { return false; }
+    bool visitUnhandledExpr(Operation *op, llvm::DenseMap<Value, double> latencyMap) { return false; }
+    // constant operation have zero-latency
+    bool visitExpr(ConstantOp op, llvm::DenseMap<Value, double> latencyMap) {
+      latencyMap[op->getOpResult(0)] = 0.0;
+      return true;
+     }
+
+    bool visitExpr(SubfieldOp op, llvm::DenseMap<Value, double> latencyMap) {
+      auto input = op.input();
+      auto it = latencyMap.find(input);
+      if (it == latencyMap.end()) return false;
+      double latency = it->second;
+      latencyMap[op->getOpResult(0)] = latency;
+      return true;
+    }
+    bool visitExpr(XorPrimOp op, llvm::DenseMap<Value, double> latencyMap) {
+      double maxLatency = 0.0;
+      for (auto operand: op->getOperands()) {
+        auto it = latencyMap.find(operand);
+        if (it == latencyMap.end()) return false;
+        double operandLatency = it->second;
+        if (operandLatency > maxLatency)
+          maxLatency = operandLatency;
+      }
+      const double latency_xor = 2.0;
+      op->getOpResult(0).dump();
+      latencyMap[op->getOpResult(0)] = maxLatency + latency_xor;
+      return true;
+    }
+    bool visitExpr(AndPrimOp op, llvm::DenseMap<Value, double> latencyMap) {
+      double maxLatency = 0.0;
+      for (auto operand: op->getOperands()) {
+        auto it = latencyMap.find(operand);
+        if (it == latencyMap.end()) return false;
+        double operandLatency = it->second;
+        if (operandLatency > maxLatency)
+          maxLatency = operandLatency;
+      }
+      const double latency_and = 2.0;
+      op->getOpResult(0).dump();
+      latencyMap[op->getOpResult(0)] = maxLatency + latency_and;
+      return true;
+    }
+    bool visitStmt(ConnectOp op, llvm::DenseMap<Value, double> latencyMap) {
+      auto it = latencyMap.find(op.src());
+      if (it == latencyMap.end()) return false;
+      double latency = it->second;
+      latencyMap[op.dest()] = latency;
+      return true;
+    }
+  };
+
+  auto latencyEvaluator = ExprLatencyEvaluator();
   while (!worklist.empty()) {
     Operation* op = worklist.front();
     LLVM_DEBUG(llvm::dbgs()
                << "Processing " << op->getName().getStringRef().str() << "\n");
     worklist.pop_front();
-    auto it = opLatency.find(op);
-    if (it == opLatency.end()) continue;
+    bool latencyFound = latencyEvaluator.dispatchExprVisitor(op, valuesLatency);
 
-    double localLatency = it->second;
-    if (localLatency >= 0.0) continue; // timing already evaluated
-    // computing latency of operands
-    bool allInputValid = true;
-    double maxInpLatency = 0.0;
-    // specialize per operation
-    double localOpLatency = 1.0;
-    for (auto inp : op->getOperands()) {
-      if (localLatency < 0) {
-        // one of the operand has no valid timing yet
-        worklist.push_back(inp.getDefiningOp());
-        allInputValid = false;
-      }
-      auto inp_it = opLatency.find(inp.getDefiningOp());
-      if (inp_it == opLatency.end()) continue;
-      if (inp_it->second > maxInpLatency) maxInpLatency = inp_it->second;
+    if (latencyFound) {
+      auto it = valuesLatency.find(op->getOpResult(0));
+      LLVM_DEBUG(llvm::dbgs()
+                << "Found latency " << it->second << " for op " << op->getName().getStringRef().str() << "\n");
+
+    } else {
+      worklist.push_back(op);
     }
-    if (!allInputValid) worklist.push_back(op);
-    else {
-      localLatency = maxInpLatency + localOpLatency;
-      opLatency[op] = localLatency;
-      if (localLatency > maxOpLatency) {
-        maxOpLatency = localLatency;
-        criticalPathEnd = op;
-      }
-    }
+
   }
 
-  LLVM_DEBUG(llvm::dbgs()
-             << "critical path's end is " << criticalPathEnd->getName().getStringRef().str() << "\n");
+  // LLVM_DEBUG(llvm::dbgs()
+  //           << "critical path's end is " << criticalPathEnd->getName().getStringRef().str() << "\n");
 
   // looking for critical path
 
